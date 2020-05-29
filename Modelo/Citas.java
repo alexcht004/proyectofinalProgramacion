4d652b063e9bd7a95f75e6ea126d9744d2a6d9de
alexcht004 <56443132+alexcht004@users.noreply.github.com>
2020-05-29 03:02:39 +0200

    Add files via upload

---
+++ Modelo/Citas.java
@@ -0,0 +1,51 @@
+
+package Modelo;
+
+
+public class Citas {
+    private int id;
+    private String descripcion,fecha;
+    private String hora;
+    private String tipo;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    
+}
