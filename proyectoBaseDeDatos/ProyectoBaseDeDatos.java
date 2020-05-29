a2bd87a2b18ea6f4e412feb5b2c0d59f319601aa
alexcht004 <56443132+alexcht004@users.noreply.github.com>
2020-05-29 03:06:38 +0200

    Add files via upload

---
+++ proyectoBaseDeDatos/ProyectoBaseDeDatos.java
@@ -0,0 +1,40 @@
+
+package proyectoBaseDeDatos;
+
+import Controlador.CtrlProducto;
+import Controlador.CtrlCliente;
+
+import Controlador.ctrlEmpleados;
+
+
+import Modelo.Cliente;
+import Modelo.Empleados;
+import Modelo.controlBaseDeDatos;
+import Modelo.Producto;
+
+
+
+import Vista.frmCliente;
+import Vista.frmEmpleado;
+
+import Vista.frmProducto;
+import Vista.menu;
+
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class ProyectoBaseDeDatos {
+    
+   
+    public static void main(String[] args){
+        
+         menu m=new menu();
+         m.main(args);
+       
+    }
+    
+}
+
+    
